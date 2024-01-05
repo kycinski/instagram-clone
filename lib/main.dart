35395aedf8d2d9c4afedf72566fecbf195fcc359
krzysztofkycia <krzysztof.kycia@ilaresoft.com>
2024-01-05 16:34:44 +0100

    fix ios build

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
